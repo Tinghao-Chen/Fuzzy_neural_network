d290c636790e0b43531641802cd061115802001f
2809872033 <108071268+2809872033@users.noreply.github.com>
2023-07-05 14:32:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,18 +60,19 @@ for epoch in range(epoch_num):
         # data.size(0)返回输入数据data的第一个维度的大小，即批次大小（batch size）。
         train_loss = loss.item() * data.size(0)
 
-    valid_loss = 0.0
+   valid_loss = 0.0
     # 将模型设置为评估模式,在训练阶段，模型会根据训练数据进行参数更新和学习，以提高模型的性能。
     # 而在评估阶段，模型主要用于对新样本进行预测，并评估模型的性能。
     model.eval()
-
     for data, labels in valid_set_loader:
         data, labels = data.to(device), labels.to(device)
         #
         target = model(data)
         loss = criterion(target, labels)
-        valid_loss = loss.item() * data.size(0)
+        valid_loss += loss.item() * data.size(0)
 
+    train_loss /= len(train_set_loader.dataset)
+    valid_loss /= len(valid_set_loader.dataset)
     print('Epoch: {:d} - training loss: {:.6f} - validation loss: {:.6f}'.format(epoch, train_loss, valid_loss))
 
 # Set the model to evaluation mode
